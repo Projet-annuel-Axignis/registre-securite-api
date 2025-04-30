7d6e5ad4e8e7f5cbb3357880557127d43cccbd0b
Loan <loanbillonnet@gmail.com>
2025-04-30 19:02:11 +0200

    docs(user): :memo: update swagger and bruno login example

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'john-doe@example.com' })
+  @ApiProperty({ example: 'admin@admin.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
