ff2574de2b6c71b7ea757239d43f02a7e7cc6789
Loan <loanbillonnet@gmail.com>
2025-05-08 11:48:14 +0200

    feat: :card_file_box: refactor migration file

--- src/orm/migrations/1746697245273-BaseEntities.ts
+++ src/orm/migrations/1746697245273-BaseEntities.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Migrations1745932702319 implements MigrationInterface {
-  name = 'Migrations1745932702319';
+export class BaseEntities1746697245273 implements MigrationInterface {
+  name = 'BaseEntities1746697245273';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
@@ -46,8 +46,8 @@ export class Migrations1745932702319 implements MigrationInterface {
             "deleted_at" TIMESTAMP,
             "name" character varying NOT NULL,
             "plan_id" integer,
-            CONSTRAINT "REL_8f8e7240de887aae547688903a" UNIQUE ("plan_id"),
-            CONSTRAINT "PK_a7a13f4cacb744524e44dfdad32" PRIMARY KEY ("id")
+            CONSTRAINT "REL_dd3d979b6c62d4e5ad3ff2fb44" UNIQUE ("plan_id"),
+            CONSTRAINT "PK_056f7854a7afdba7cbd6d45fc20" PRIMARY KEY ("id")
         )
     `);
     await queryRunner.query(`
@@ -81,7 +81,7 @@ export class Migrations1745932702319 implements MigrationInterface {
             "email" character varying NOT NULL,
             "password" character varying NOT NULL,
             "role_id" integer,
-            "customer_id" integer,
+            "company_id" integer,
             CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"),
             CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
         )
@@ -92,7 +92,7 @@ export class Migrations1745932702319 implements MigrationInterface {
     `);
     await queryRunner.query(`
         ALTER TABLE "company"
-        ADD CONSTRAINT "FK_8f8e7240de887aae547688903ae" FOREIGN KEY ("plan_id") REFERENCES "plan"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+        ADD CONSTRAINT "FK_dd3d979b6c62d4e5ad3ff2fb44a" FOREIGN KEY ("plan_id") REFERENCES "plan"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
     `);
     await queryRunner.query(`
         ALTER TABLE "user"
@@ -100,29 +100,29 @@ export class Migrations1745932702319 implements MigrationInterface {
     `);
     await queryRunner.query(`
         ALTER TABLE "user"
-        ADD CONSTRAINT "FK_d72eb2a5bbff4f2533a5d4caff9" FOREIGN KEY ("customer_id") REFERENCES "company"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+        ADD CONSTRAINT "FK_9e70b5f9d7095018e86970c7874" FOREIGN KEY ("company_id") REFERENCES "company"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
     `);
 
     // Add roles in DB
     await queryRunner.query(`
         INSERT INTO "role" ("name", "type", "description") VALUES
-          ('Administrateur', 'ADMINISTRATOR', 'Administrateur possédant un accès total à l''application'),
-          ('Administrateur de société', 'COMPANY_ADMINISTRATOR', 'Gestionnaire d''un compte Client'),
-          ('Manager de société', 'COMPANY_MANAGER', 'Compte client possédant plus de privilèges qu''un simple client'),
-          ('Simple membre', 'COMPANY_MEMBER', 'Compte client standard'),
-          ('Visiteur', 'VISITOR', 'Compte en lecture seule');
-      `);
+            ('Administrateur', 'ADMINISTRATOR', 'Administrateur possédant un accès total à l''application'),
+            ('Administrateur de société', 'COMPANY_ADMINISTRATOR', 'Gestionnaire d''un compte Client'),
+            ('Manager de société', 'COMPANY_MANAGER', 'Compte client possédant plus de privilèges qu''un simple client'),
+            ('Simple membre', 'COMPANY_MEMBER', 'Compte client standard'),
+            ('Visiteur', 'VISITOR', 'Compte en lecture seule');
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        ALTER TABLE "user" DROP CONSTRAINT "FK_d72eb2a5bbff4f2533a5d4caff9"
+        ALTER TABLE "user" DROP CONSTRAINT "FK_9e70b5f9d7095018e86970c7874"
     `);
     await queryRunner.query(`
         ALTER TABLE "user" DROP CONSTRAINT "FK_fb2e442d14add3cefbdf33c4561"
     `);
     await queryRunner.query(`
-        ALTER TABLE "company" DROP CONSTRAINT "FK_8f8e7240de887aae547688903ae"
+        ALTER TABLE "company" DROP CONSTRAINT "FK_dd3d979b6c62d4e5ad3ff2fb44a"
     `);
     await queryRunner.query(`
         ALTER TABLE "activity_log" DROP CONSTRAINT "FK_81615294532ca4b6c70abd1b2e6"
--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompanyController } from './controllers/company.controller';
 import { UserController } from './controllers/user.controller';
 import { Company } from './entities/company.entity';
 import { Plan } from './entities/plan.entity';
 import { Role } from './entities/role.entity';
 import { User } from './entities/user.entity';
+import { CompanyService } from './services/company.service';
 import { UserService } from './services/user.service';
 
 @Module({
-  controllers: [UserController],
+  controllers: [UserController, CompanyController],
   imports: [TypeOrmModule.forFeature([User, Role, Company, Plan])],
-  providers: [UserService],
+  providers: [UserService, CompanyService],
   exports: [UserService],
 })
 export class UserModule {}
