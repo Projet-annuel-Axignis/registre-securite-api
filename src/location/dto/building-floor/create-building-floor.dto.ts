a177b57094283a6832064873837f2b97d0522dee
Loan <loanbillonnet@gmail.com>
2025-06-29 19:17:27 +0200

    docs(location module): :memo: update swagger

--- src/location/dto/building-floor/create-building-floor.dto.ts
+++ src/location/dto/building-floor/create-building-floor.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateBuildingFloorDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsInt()
   buildingId: number;
 }
--- src/location/dto/building/create-building.dto.ts
+++ src/location/dto/building/create-building.dto.ts
@@ -1,26 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IghClassCode } from '@src/location/types/igh-class.types';
 import { TypologyCode } from '@src/location/types/typology-code.types';
 import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, ValidateIf } from 'class-validator';
 
 export class CreateBuildingDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsInt()
   siteId: number;
 
+  @ApiProperty()
   @IsEnum(TypologyCode, { each: true })
   typologyCodes: TypologyCode[];
 
+  @ApiProperty()
   @ValidateIf((dto: CreateBuildingDto) => dto.typologyCodes.includes(TypologyCode.IGH))
   @IsEnum(IghClassCode, { each: true })
   ighClassCodes: IghClassCode[];
 
+  @ApiProperty()
   @ValidateIf((dto: CreateBuildingDto) => dto.typologyCodes.includes(TypologyCode.ERP))
   @IsInt({ each: true })
   erpCategory: number;
 
+  @ApiProperty()
   @IsInt({ each: true })
   @IsOptional()
   authorizedUserIds?: number[];
